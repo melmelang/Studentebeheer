9d4ffe618b66697ffae842dbe6faa3253543dcbc
melmelang <74968643+melmelang@users.noreply.github.com>
2021-11-30 13:51:09 +0100

    abdel

---
+++ Studentenbeheer/Studentenbeheer/Data/StudentenbeheerContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Studentenbeheer.Models;
+
+namespace Studentenbeheer.Data
+{
+    public class StudentenbeheerContext : DbContext
+    {
+        public StudentenbeheerContext (DbContextOptions<StudentenbeheerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Studentenbeheer.Models.Student> Student { get; set; }
+    }
+}
---
+++ Studentenbeheer/Studentenbeheer/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Studentenbeheer.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Voornaam { get; set; }
+        public string Achternaam { get; set; }
+
+        [DataType (DataType.Date)]
+        public DateTime Geboortedatum { get; set; }
+        public char Geslacht { get; set; }
+    }
+}
